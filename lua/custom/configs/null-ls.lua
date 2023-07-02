cf6184fd7a3254ba5d6cddbd2654968d1a158468
Rushabh Kanadiya <rushabhkanadia@gmail.com>
2023-07-02 20:44:48 +0530

    add autoformatting

--- lua/custom/configs/null-ls.lua
+++ lua/custom/configs/null-ls.lua
@@ -8,7 +8,21 @@ local sources = {
   formatting.clang_format
 }
 
+
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 null_ls.setup {
   debug = true,
-  sources = sources
+  sources = sources,
+  on_attach = function(client, bufnr)
+    if client.supports_method("textDocument/formatting") then
+      vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+      vim.api.nvim_create_autocmd("BufWritePre" , {
+        group = augroup,
+        buffer = bufnr,
+        callback = function()
+            vim.lsp.buf.format({ async = false })
+        end
+      })
+    end
+  end
 }
